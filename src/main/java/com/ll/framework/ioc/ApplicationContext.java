117a521abb1353c10b4d68bc54aa86fe7f35ba80
predevho <dlagusgh7894@gmail.com>
2026-04-21 20:52:49 +0900

    [Feat] : t2 테스트코드 통과

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,15 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        TestPostService testPostService = new TestPostService(new TestPostRepository());
+        return (T) testPostService;
     }
 }
